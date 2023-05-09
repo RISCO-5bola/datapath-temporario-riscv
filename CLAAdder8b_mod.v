ae4848518d63e363c43273964d70c06f9ba363a3
Lucas Suzin Bertan <bertan1707@gmail.com>
2023-05-08 21:47:42 -0300

    Sending all updates

--- CLAAdder8b_mod.v
+++ CLAAdder8b_mod.v
@@ -1,15 +1,3 @@
-/* Módulo do somador Carry Look-Ahead de 8bits.
-Recebe dois números de 8 bits, A e B e um carry in CIN e devolve a soma A + B no S, assim como o carry out dela no COUT.
-O somador carry look ahead calcula os carries fazendo 
-Ci = Gi + Pi*Ci-1 
-sendo:
-    Gi = Ai * Bi (generate, verifica se Ai e Bi geram um carry)
-    Pi =  Ai + Bi (propagate, verifica se Ai e Bi propagam um carry)
-    Ci-1 = carry anterior
-
-cada carry i é somado com os bits Ai e Bi no somador parcial de um bit (PartialFullAdder1b)
-*/
-
 `include "PartialFullAdder1b_mod.v"
 
 module CLAAdder8b(A, B, CIN, S, COUT);
@@ -23,39 +11,30 @@ module CLAAdder8b(A, B, CIN, S, COUT);
     wire PC1, PC2, PC3, PC4, PC5, PC6, PC7, PC8;
     wire G1, G2, G3, G4, G5, G6, G7, G8;
     
-    // C1 = G1 + P1*C0 (C0 = CIN) 
     and P01 (PC1,P0,CIN);
     or C01 (C1, G1, PC1);
 
-    // C2 = G2 + P2*C1 
     and P02 (PC2,P1, C1);
     or C02 (C2, G2, PC2);
 
-    // C3 = G3 + P3*C2 
     and P03 (PC3, P2, C2);
     or C03 (C3, G3, PC3);
 
-    // C4 = G4 + P4*C3 
     and P04 (PC4, P3, C3);
     or C04 (C4, G4, PC4);
 
-    // C5 = G5 + P5*C4 
     and P05 (PC5, P4, C4);
     or C05 (C5, G5, PC5);
 
-    // C6 = G6 + P6*C5 
     and P06 (PC6, P5, C5);
     or C06 (C6, G6, PC6);
 
-    // C7 = G7 + P7*C6 
     and P07 (PC7, P6, C6);
     or C07 (C7, G7, PC7);
 
-    // C8 = G8 + P8*C7 
     and P08 (PC8, P7, C7);
     or C08 (COUT, G8, PC8);
 
-    //Sequencia de Partial Full Adders, um para cada bit de A e B
     PartialFullAdder1b U0 (.A(A[0]), .B(B[0]), .CIN(CIN), .S(S[0]), .P(P0), .G(G1));
     PartialFullAdder1b U1 (.A(A[1]), .B(B[1]), .CIN(C1), .S(S[1]), .P(P1), .G(G2));
     PartialFullAdder1b U2 (.A(A[2]), .B(B[2]), .CIN(C2), .S(S[2]), .P(P2), .G(G3));
--- datapath.v
+++ datapath.v
@@ -8,6 +8,7 @@
 `include "Adder64b_mod.v"
 `include "mux_2x1_64bit.v"
 `include "mux_2x1_64b_ImOrData2.v"//O Mux que seleciona entre immediate e o data2 para mandar para o somador de 64 bits.
+`include "mux6x1_1b.v"
 
 // a implementacao se baseou no fluxo de dados do
 // proprio risc-v
@@ -17,12 +18,14 @@ module datapath (
     input [4:0] readRegister1, // o endereco do qual se lê o "x2"
     input [4:0] readRegister2, // o endereco do qual se tira o "x1"
     input [4:0] writeRegister, // o endereco no qual se salva o valor no registrador
+    input [2:0] funct3,
     input writeEnable_DataMemory,
     input writeEnable_Registers,
     input muxSelect_SumVsReadData,
     input muxSelect_ImmVsDataout2, //Sinal mandando para o mux2 selecionar entre immediate (operacao S) ou dataout2 (na operacao aritmetica)
     input SumOrSub, //Sinal mandando para o somador/subtrador. - quando 1 indica a subtracao
-    input clk
+    input clk,
+    output selectedFlag
 );
     //sinais e instanciacao do banco de registradores;
 
@@ -33,28 +36,35 @@ module datapath (
     wire[63:0] result_soma; //endereco do data memory especificado pela soma obtida do somador.
     wire[63:0] result_selection_mux;
     wire[63:0] result_selection_mux_2;
+
+    /*
+        WIRE PARA AS FLAGS
+    */
+    wire [5:0] flags;
     
     registradores registradores(.readRegister1(readRegister1), .readRegister2(readRegister2),
     .writeRegister(writeRegister), .writeData(result_selection_mux), .regWrite(writeEnable_Registers),
     .clk(clk), .readData1(dataOut1), .readData2(dataOut2));
 
-    //sinais e instanciacao do datamemory
-    //aqui a ideia eh salvar o valor de x1 no x2 + immediate;
+    // sinais e instanciacao do datamemory
+    // aqui a ideia eh salvar o valor de x1 no x2 + immediate;
 
     data_memory data_memory(.clk(clk), .mem_read(1'b1), .mem_write(writeEnable_DataMemory),
     .endereco(result_soma[7:0]), .write_data(dataOut2), .read_data(dataWriteOnRegisterBank));
 
-    //instanciacao do somador de 64 bits;
-
+    /* Trocar pela ALU */
     Adder64b_mod Adder64_mod(.A(dataOut1), .B(result_selection_mux_2), .SUB(SumOrSub), .S(result_soma));
 
-    //instanciando o mux;
+    // instanciando o mux;
     mux_2x1_64bit mux_2x1_64bit(.S(muxSelect_SumVsReadData), .A(dataWriteOnRegisterBank),
     .B(result_soma), .X(result_selection_mux));
 
-    //instanciando o mux2:
+    // instanciando o mux2:
     mux_2x1_64b_ImOrData2 mux_2x1_64b_ImOrData2(.SelectImOrData2(muxSelect_ImmVsDataout2), .Immediate(immediate),
     .DataOut2(dataOut2), .X(result_selection_mux_2));
 
+    // mux seleciona flag
+    mux6x1_1b mux6x1_1b (.BEQ(flags[5]), .BNE(flags[4]), .BLT(flags[3]),
+                         .BGE(flags[2]), .BLTU(flags[1]), .BGEU(flags[0]),
+                         .funct3(funct3), .selectedFlag(selectedFlag));
 endmodule
-
